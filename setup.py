c05d8ba97aed938cf7a6bc3cf1b372b8b2835b4b
Marcel Arpogaus <marcel@arpogaus.de>
2020-10-14 21:20:43 +0200

    added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="bernstein_flow", packages=find_packages('src'), package_dir={"": "src"})
+setup(
+    name="bernstein_flow",
+    version="0.0.1",
+    packages=find_packages('src'),
+    package_dir={"": "src"},
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'tensorflow',
+        'tensorflow_probability',
+    ]
+)
