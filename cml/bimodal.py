e3ecdbac2a0904ab7fca91cfd1e89eb6f0b9c9e4
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-05-07 17:05:11 +0200

    adds parameters and mlflow tracking to  bimodal script

--- cml/bimodal.py
+++ cml/bimodal.py
@@ -5,37 +5,41 @@
 # author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 #
 # created : 2021-03-22 16:42:31 (Marcel Arpogaus)
-# changed : 2021-04-30 18:53:50 (Marcel Arpogaus)
+# changed : 2021-05-07 17:01:01 (Marcel Arpogaus)
 # DESCRIPTION ##################################################################
 # ...
 # LICENSE ######################################################################
 # ...
 ################################################################################
-
+import argparse
+import os
+import sys
 from functools import partial
-import pandas as pd
-import numpy as np
 
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow_probability as tfp
-
-from tensorflow_probability import distributions as tfd
-from tensorflow_probability import bijectors as tfb
-
+import yaml
+from bernstein_flow.distributions import BernsteinFlow
+from bernstein_flow.util.visualization import (
+    plot_chained_bijectors,
+    plot_value_and_gradient,
+    plot_x_trafo,
+    vizualize_flow_from_z_domain,
+)
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Sequential
+from tensorflow_probability import bijectors as tfb
+from tensorflow_probability import distributions as tfd
 
-from bernstein_flow.bijectors import BernsteinBijector
-from bernstein_flow.distributions import BernsteinFlow
-from bernstein_flow.util.visualization import vizualize_flow_from_z_domain
+try:
+    import mlflow
 
-# Ensure Reproducibility
-np.random.seed(2)
-tf.random.set_seed(2)
-print("TFP Version", tfp.__version__)
-print("TF  Version", tf.__version__)
+    USE_MLFLOW = True
+except ImportError:
+    USE_MLFLOW = False
 
 
 def print_param(b, indent=0, prefix=""):
@@ -55,8 +59,8 @@ def print_param(b, indent=0, prefix=""):
         s += f"{b.name}: {b.scale}\n"
     elif isinstance(b, tfb.Shift):
         s += f"{b.name}: {b.shift}\n"
-    elif isinstance(b, BernsteinBijector):
-        s += f"{b.name}: {b.theta}\n"
+    elif hasattr(b, "thetas"):
+        s += f"{b.name}: {b.thetas}\n"
     elif isinstance(b, (tfb.Sigmoid, tfb.SoftClip)):
         s += f"{b.name}: {b.high}, {b.low}\n"
     else:
@@ -77,6 +81,10 @@ def gen_data(t):
     y = np.concatenate([y1, y2])
 
     return t[..., np.newaxis], y[..., np.newaxis]
+
+
+def scale_data(y, min_y, max_y):
+    return (y - min_y) / (max_y - min_y)
 
 
 def gen_test_data(n=5, observations=100):
@@ -108,9 +116,7 @@ def gen_model(bernstein_order=9, **kwds):
     )
 
     def bf(y_pred):
-        return BernsteinFlow.from_pvector(
-            y_pred, allow_values_outside_support=True, **kwds
-        )
+        return BernsteinFlow.from_pvector(y_pred, **kwds)
 
     def my_loss_fn(y_true, y_pred):
         return -tfd.Independent(bf(y_pred)).log_prob(tf.squeeze(y_true))
@@ -123,13 +129,14 @@ def gen_model(bernstein_order=9, **kwds):
     return flow_parameter_model, bf
 
 
-def fit_model(**kwds):
+def fit_model(train_x, train_y, val_x, val_y, batch_size=32, epochs=1000, **kwds):
+    lr_patience = 15
     callbacks = [
         tf.keras.callbacks.ReduceLROnPlateau(
-            monitor="val_loss", factor=0.1, patience=3
+            monitor="val_loss", factor=0.1, patience=lr_patience
         ),
         tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss", patience=9, restore_best_weights=True
+            monitor="val_loss", patience=3 * lr_patience + 10, restore_best_weights=True
         ),
         tf.keras.callbacks.TerminateOnNaN(),
     ] + kwds.pop("callbacks", [])
@@ -138,161 +145,152 @@ def fit_model(**kwds):
         train_x,
         train_y,
         validation_data=(val_x, val_y),
-        epochs=300,
+        epochs=epochs,
         shuffle=True,
-        batch_size=32,
+        batch_size=batch_size,
         callbacks=callbacks,
     )
     return model, bf, hist
 
 
-def plot_dists(model, bf):
+def plot_dists(model, bf, test_x, test_t, test_y):
     flow = bf(model(test_x[..., None]))
     yy = np.linspace(-1, 1.5, 1000, dtype=np.float32)[..., None]
     ps = flow.prob(yy)
 
-    fig, ax = plt.subplots(len(test_x), figsize=(16, len(test_x) * 4))
+    fig, ax = plt.subplots(
+        len(test_x), figsize=(10, len(test_x) * 3), constrained_layout=True
+    )
+    fig.suptitle("Learned Distributions", fontsize=16)
+
     for i, x in enumerate(test_x):
         ax[i].set_title(f"x={x}")
         sampl = test_y[(test_t.flatten() == x)].flatten()
         ax[i].scatter(sampl, [0] * len(sampl), marker="|")
-        # ax[i].hist(sampl, bins=30, density=True)
-        ax[i].plot(yy, ps[:, i])
-
+        ax[i].plot(yy, ps[:, i], label="flow")
+        ax[i].set_xlabel("y")
+        ax[i].set_ylabel(f"p(y|x={x})")
+        ax[i].legend()
     return fig
 
 
-def plot_chained_bijectors(flow):
-    chained_bijectors = flow.bijector.bijector.bijectors
-    base_dist = flow.distribution
-    cols = len(chained_bijectors) + 1
-    fig, ax = plt.subplots(1, cols, figsize=(4 * cols, 4))
+def run(params, metrics_path, artifact_path):
+    # Data
+    n = 100 if args._10sec else 4000
+    train_x, train_y = gen_train_data(n=n)
+    val_x, val_y = gen_train_data(n=n // 10)
+    train_x.shape, train_y.shape, val_x.shape, val_y.shape
+    test_t, test_y = gen_test_data(5, 200)
+    test_x = np.unique(test_t)
 
-    n = 200
+    if params.get("scale_data", False):
+        min_y, max_y = train_y.min(), train_y.max()
+        train_y = scale_data(train_y, min_y, max_y)
+        val_y = scale_data(val_y, min_y, max_y)
+        test_y = scale_data(test_y, min_y, max_y)
 
-    z_samples = np.linspace(-3, 3, n).astype(np.float32)
-    log_probs = base_dist.log_prob(z_samples)
+    # Fit Model
+    model, bf, hist = fit_model(train_x, train_y, val_x, val_y, **params["fit_kwds"])
 
-    ax[0].plot(z_samples, np.exp(log_probs))
+    # Results
+    fig = plt.figure(figsize=(16, 8))
+    plt.scatter(train_x, train_y, alpha=0.5, label="train")
+    plt.scatter(test_t, test_y, alpha=0.5, label="test")
+    plt.scatter(val_x, val_y, alpha=0.5, label="test")
 
-    zz = z_samples[..., None]
-    ildj = 0.0
-    for i, (a, b) in enumerate(zip(ax[1:], chained_bijectors)):
-        # we need to use the inverse here since we are going from z->y!
-        z = b.inverse(zz)
-        ildj += b.forward_log_det_jacobian(z, 1)
-        # print(z.shape, zz.shape, ildj.shape)
-        a.scatter(z, np.exp(log_probs + ildj))
-        a.set_title(b.name.replace("_", " "))
-        a.set_xlabel(f"$z_{i}$")
-        a.set_ylabel(f"$p(z_{i+1})$")
-        zz = z
-    fig.tight_layout()
-    return fig
+    plt.legend()
+    fig.savefig(os.path.join(artifacts_path, "bm_data.png"))
 
+    fig, axes = plt.subplots(2, figsize=(16, 8))
+    hist_df = pd.DataFrame(hist.history)
 
-def plot_x_trafo(flow, xmin=-1, xmax=1, n=20, size=4):
-    x = np.linspace(xmin, xmax, n, dtype=np.float32)
-    num_bij = len(flow.bijector.bijector.bijectors)
-    fig, ax = plt.subplots(2, num_bij, figsize=(size * num_bij, size * 2))
-    for i, b in enumerate(reversed(flow.bijector.bijector.bijectors)):
-        y = b.forward(x)
-        ax[0, i].scatter(x, y, alpha=0.2)
-        ax[0, i].set_title(
-            b.name.replace("_", " ").title().replace("Bernsteinflow", "")
-        )
-        x = y
-    # y = np.linspace(min(y),max(y), 400, dtype=np.float32)
-    for i, b in enumerate(flow.bijector.bijector.bijectors):
-        x = b.inverse(tf.identity(y))
-        ax[1, num_bij - i - 1].scatter(x, y, alpha=0.2)
-        ax[1, num_bij - i - 1].set_title(
-            b.name.replace("_", " ").title().replace("Bernsteinflow", "")
-        )
-        y = x
+    fig = hist_df[["loss", "val_loss"]].plot(ax=axes[0]).get_figure()
+    hist_df[["lr"]].plot(ax=axes[1])
+    fig.savefig(os.path.join(artifacts_path, "bm_hist.png"))
 
-    return fig
+    with open(os.path.join(metrics_path, "bm_metrics.yaml"), "w") as metrics:
+        min_loss = hist_df[["loss", "val_loss"]].min().to_dict()
+        yaml.dump(min_loss, metrics)
 
+    fig = plot_dists(model, bf, test_x, test_t, test_y)
+    fig.savefig(os.path.join(artifacts_path, "bm_dists.png"))
 
-def plot_value_and_gradient(func, y):
-    [funval, grads] = tfp.math.value_and_gradient(func, y)
+    flow = bf(model(tf.ones((1, 1))))
 
-    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
-    ax[0].scatter(y, funval, s=1, label="funval")
-    ax[0].legend()
-    ax[1].scatter(y, grads, s=1, label="grad")
-    ax[1].legend()
-    return fig
+    with open(os.path.join(artifact_path, "bm_pvector.txt"), "w") as pvector:
+        pvector.write(print_param(flow))
 
+    fig = vizualize_flow_from_z_domain(flow)
+    fig.savefig(os.path.join(artifacts_path, "bm_flow.png"))
 
-# Data
+    # Bijector
+    fig = plot_x_trafo(flow, xmin=-2, xmax=2, n=25)
+    fig.savefig(os.path.join(artifacts_path, "bm_bijectors.png"))
 
+    fig = plot_chained_bijectors(flow)
+    fig.savefig(os.path.join(artifacts_path, "bm_trafo.png"))
 
-n = 4000
-train_x, train_y = gen_train_data(n=n)
-val_x, val_y = gen_train_data(n=n // 10)
-train_x.shape, train_y.shape, val_x.shape, val_y.shape
-test_t, test_y = gen_test_data(5, 200)
-test_x = np.unique(test_t)
+    y = np.linspace(-3, 3, 1000, dtype=np.float32)
+    fig = plot_value_and_gradient(flow.bijector.inverse, y.copy())
+    fig.savefig(os.path.join(artifacts_path, "bm_bijector.png"))
 
-fig = plt.figure(figsize=(16, 8))
-plt.scatter(train_x, train_y, alpha=0.5, label="train")
-plt.scatter(test_t, test_y, alpha=0.5, label="test")
-plt.scatter(val_x, val_y, alpha=0.5, label="test")
+    def fun(y):
+        return flow.bijector.inverse_log_det_jacobian(y, 0)
 
-plt.legend()
-fig.savefig("bm_data.png")
-
-# Fit Model
-
-model, bf, hist = fit_model(bernstein_order=20, scale_base_distribution=True)
+    fig = plot_value_and_gradient(fun, y.copy())
+    fig.savefig(os.path.join(artifacts_path, "bm_ildj.png"))
 
 
-# Results
-result_path = "metrics/"
-figure, axes = plt.subplots(2, figsize=(16, 8))
-hist_df = pd.DataFrame(hist.history)
-hist_df.to_csv(result_path + "bm_hist.csv")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--10sec", help="run faster", action="store_true", dest="_10sec"
+    )
+    parser.add_argument(
+        "--no-mlflow",
+        help="disable mlfow tracking",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument("--seed", help="random seed", default=1, type=int)
 
-fig = hist_df[["loss", "val_loss"]].plot(ax=axes[0]).get_figure()
-hist_df[["lr"]].plot(ax=axes[1])
-fig.savefig(result_path + "bm_hist.png")
+    args = parser.parse_args()
+    params = yaml.safe_load(open("cml/params.yaml"))["bimodal"]
 
-with open(result_path + "bm_metrics.txt", "w") as metrics:
-    metrics.write("loss: " + str(hist_df.loss.min()) + "\n")
-    metrics.write("val loss: " + str(hist_df.val_loss.min()) + "\n")
+    # Ensure Reproducibility
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    print("TFP Version", tfp.__version__)
+    print("TF  Version", tf.__version__)
 
-fig = plot_dists(model, bf)
-fig.savefig(result_path + "bm_dists.png")
+    metrics_path = "metrics/bimodal"
+    artifacts_path = "artifacts/bimodal"
 
-flow = bf(model(tf.ones((1, 1))))
+    if not os.path.exists(metrics_path):
+        os.makedirs(metrics_path)
 
+    if not os.path.exists(artifacts_path):
+        os.makedirs(artifacts_path)
 
-with open(result_path + "bm_pvector.txt", "w") as pvector:
-    pvector.write(print_param(flow))
+    if args._10sec:
+        params["fit_kwds"].update({"epochs": 1})
 
-fig = vizualize_flow_from_z_domain(flow)
-fig.savefig(result_path + "bm_flow.png")
+    if USE_MLFLOW and not args.no_mlflow:
+        print("mlflow tracking enabled")
+        mlflow.autolog()
+        experiment_id = mlflow.set_experiment("bernstein_bimodal")
+        if os.environ.get("MLFLOW_RUN_ID", False):
+            mlflow.start_run()
+        with mlflow.start_run(
+            experiment_id=experiment_id, nested=mlflow.active_run() is not None
+        ):
 
-
-# Bijector
-
-
-fig = plot_x_trafo(flow, xmin=-2, xmax=2, n=25)
-fig.savefig(result_path + "bm_bijectors.png")
-
-fig = plot_chained_bijectors(flow)
-fig.savefig(result_path + "bm_trafo.png")
-
-
-y = np.linspace(-3, 3, 1000, dtype=np.float32)
-fig = plot_value_and_gradient(flow.bijector.inverse, y.copy())
-fig.savefig(result_path + "bm_bijector.png")
-
-
-def fun(y):
-    return flow.bijector.inverse_log_det_jacobian(y, 0)
-
-
-fig = plot_value_and_gradient(fun, y.copy())
-fig.savefig(result_path + "bm_ildj.png")
+            mlflow.log_param("seed", args.seed)
+            mlflow.log_params(
+                dict(filter(lambda kw: not isinstance(kw[1], dict), params.items()))
+            )
+            mlflow.log_params(params["fit_kwds"])
+            run(params, metrics_path, artifacts_path)
+            mlflow.log_artifacts(artifacts_path)
+    else:
+        run(params, metrics_path, artifacts_path)
--- src/bernstein_flow/util/visualization.py
+++ src/bernstein_flow/util/visualization.py
@@ -33,6 +33,9 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.patches import ConnectionPatch
 
+import tensorflow as tf
+import tensorflow_probability as tfp
+
 import numpy as np
 
 
@@ -62,20 +65,20 @@ def vizualize_flow_from_z_domain(flow, z_min=-3, z_max=3):
     gs1 = gridspec.GridSpec(2, 2)
 
     b_ax = fig.add_subplot(gs1[0, 1])
-    b_ax.spines['top'].set_color('none')  # don't draw spine
-    b_ax.spines['right'].set_color('none')  # don't draw spine
-    b_ax.spines['bottom'].set_position(('outward', 10))
-    b_ax.spines['left'].set_position(('outward', 10))
+    b_ax.spines["top"].set_color("none")  # don't draw spine
+    b_ax.spines["right"].set_color("none")  # don't draw spine
+    b_ax.spines["bottom"].set_position(("outward", 10))
+    b_ax.spines["left"].set_position(("outward", 10))
 
-    b_ax.set_xlabel('z')
-    b_ax.set_ylabel('y')
+    b_ax.set_xlabel("z")
+    b_ax.set_ylabel("y")
 
     y_ax = fig.add_subplot(gs1[0, 0], sharey=b_ax)
-    y_ax.axis('off')
+    y_ax.axis("off")
     plt.gca().invert_xaxis()
 
     z_ax = fig.add_subplot(gs1[1, 1], sharex=b_ax)
-    z_ax.axis('off')
+    z_ax.axis("off")
     plt.gca().invert_yaxis()
 
     gs1.tight_layout(fig)
@@ -89,35 +92,31 @@ def vizualize_flow_from_z_domain(flow, z_min=-3, z_max=3):
     mu_y = bijector.forward(mu_z)
 
     p_mu_z = base_dist.prob(mu_z)
-    p_mu_y = np.exp(base_dist.log_prob(mu_z) +
-                    bijector.inverse_log_det_jacobian(mu_y, 0))
-
-    cp_kwds = dict(
-        color='darkgray',
-        lw=1,
-        ls='--',
-        arrowstyle='->'
+    p_mu_y = np.exp(
+        base_dist.log_prob(mu_z) + bijector.inverse_log_det_jacobian(mu_y, 0)
     )
+
+    cp_kwds = dict(color="darkgray", lw=1, ls="--", arrowstyle="->")
 
     project_y = ConnectionPatch(
         xyA=(mu_z, p_mu_z),
         xyB=(mu_z, mu_y),
-        coordsA='data',
-        coordsB='data',
+        coordsA="data",
+        coordsB="data",
         axesA=z_ax,
         axesB=b_ax,
-        **cp_kwds
+        **cp_kwds,
     )
     z_ax.add_artist(project_y)
 
     project_y = ConnectionPatch(
         xyA=(mu_z, mu_y),
         xyB=(p_mu_y, mu_y),
-        coordsA='data',
-        coordsB='data',
+        coordsA="data",
+        coordsB="data",
         axesA=b_ax,
         axesB=y_ax,
-        **cp_kwds
+        **cp_kwds,
     )
     b_ax.add_artist(project_y)
 
@@ -128,7 +127,8 @@ def plot_chained_bijectors(flow):
     chained_bijectors = flow.bijector.bijector.bijectors
     base_dist = flow.distribution
     cols = len(chained_bijectors) + 1
-    fig, ax = plt.subplots(1, cols, figsize=(4 * cols, 4))
+    fig, ax = plt.subplots(1, cols, figsize=(3 * cols, 3), constrained_layout=True)
+    fig.suptitle("Chain of Transformations", fontsize=16)
 
     n = 200
 
@@ -138,80 +138,152 @@ def plot_chained_bijectors(flow):
     ax[0].plot(z_samples, np.exp(log_probs))
 
     zz = z_samples[..., None]
-    ildj = 0.
+    ildj = 0.0
     for i, (a, b) in enumerate(zip(ax[1:], chained_bijectors)):
         # we need to use the inverse here since we are going from z->y!
         z = b.inverse(zz)
         ildj += b.forward_log_det_jacobian(z, 1)
         # print(z.shape, zz.shape, ildj.shape)
-        a.plot(z, np.exp(log_probs + ildj))
-        a.set_title(b.name.replace('_', ' '))
-        a.set_xlabel(f'$z_{i}$')
-        a.set_ylabel(f'$p(z_{i+1})$')
+        a.scatter(z, np.exp(log_probs + ildj))
+        a.set_title(b.name.replace("_", " "))
+        a.set_xlabel(f"$z_{i}$")
+        a.set_ylabel(f"$p(z_{i+1})$")
         zz = z
-    fig.tight_layout()
+    return fig
 
 
-def plot_flow(flow, y, ax=plt, color='skyblue'):
+def plot_x_trafo(flow, xmin=-1, xmax=1, n=20, size=3):
+    x = np.linspace(xmin, xmax, n, dtype=np.float32)
+    pos = n // 2
+    con_kwds = dict(
+        arrowstyle="-|>",
+        shrinkA=8,
+        shrinkB=8,
+        mutation_scale=15,
+        color="gray",
+        ls="--",
+        connectionstyle="arc3,rad=-0.08",
+    )
+    num_bij = len(flow.bijector.bijector.bijectors)
+    fig, ax = plt.subplots(
+        2, num_bij, figsize=(size * num_bij, size * 2), constrained_layout=True
+    )
+    fig.suptitle("y-Transformations", fontsize=16)
+
+    for i, b in enumerate(reversed(flow.bijector.bijector.bijectors)):
+        y = b.forward(x)
+        ax[0, i].scatter(x, y)
+        ax[0, i].set_title(
+            b.name.replace("_", " ").title().replace("Bernsteinflow", "")
+        )
+        if i == 0:
+            ax[0, i].set_xlabel(f"$y$")
+        else:
+            ax[0, i].set_xlabel(f"$z_{i-1}$")
+
+            con = ConnectionPatch(
+                xyA=[x_old[pos], y_old[pos]],
+                coordsA=ax_old.transData,
+                xyB=[y_old[pos], y[pos]],
+                coordsB=ax[0, i].transData,
+                **con_kwds,
+            )
+
+            fig.add_artist(con)
+        ax[0, i].set_ylabel(f"$z_{i}$")
+        y_old = y
+        x_old = x
+        ax_old = ax[0, i]
+        x = y
+    for i, b in enumerate(flow.bijector.bijector.bijectors):
+        x = b.inverse(tf.identity(y))
+        ax[1, num_bij - i - 1].scatter(x, y)
+        ax[1, num_bij - i - 1].set_title(
+            b.name.replace("_", " ").title().replace("Bernsteinflow", "")
+        )
+        if num_bij - i - 1 == 0:
+            ax[1, num_bij - i - 1].set_xlabel(f"$y$")
+        else:
+            ax[1, num_bij - i - 1].set_xlabel(f"$z_{num_bij - i - 2}$")
+        con = ConnectionPatch(
+            xyA=[x_old[pos], y_old[pos]],
+            coordsA=ax_old.transData,
+            xyB=[x[pos], y[pos]],
+            coordsB=ax[1, num_bij - i - 1].transData,
+            **con_kwds,
+        )
+
+        fig.add_artist(con)
+
+        ax[1, num_bij - i - 1].set_ylabel(f"$z_{num_bij - i - 1}$")
+        y_old = y
+        x_old = x
+        ax_old = ax[1, num_bij - i - 1]
+        y = x
+    return fig
+
+
+def plot_value_and_gradient(func, y):
+    [funval, grads] = tfp.math.value_and_gradient(func, y)
+
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8), constrained_layout=True)
+    fig.suptitle(f"Value and Gradient of {func.__name__}", fontsize=16)
+
+    ax[0].scatter(y, funval, s=1, label="funval")
+    ax[0].legend()
+    ax[1].scatter(
+        y, np.gradient(funval, np.diff(y).mean()), s=1, label="np.gradient(funval)"
+    )
+    ax[1].scatter(y, grads, s=1, label="grad")
+    ax[1].legend()
+    return fig
+
+
+def plot_flow(flow, y, ax=plt, color="skyblue"):
     base_dist = flow.distribution
     bijector = flow.bijector
 
     dense_y = flow.prob(y).numpy()
 
     mu = np.squeeze(bijector.forward(base_dist.mean()))
-    plus_sd = np.squeeze(bijector.forward(
-        base_dist.mean() + base_dist.stddev()))
-    minus_sd = np.squeeze(bijector.forward(
-        base_dist.mean() - base_dist.stddev()))
+    plus_sd = np.squeeze(bijector.forward(base_dist.mean() + base_dist.stddev()))
+    minus_sd = np.squeeze(bijector.forward(base_dist.mean() - base_dist.stddev()))
 
     ax.plot(
         [mu, mu],
-        [np.min(dense_y),
-         flow.prob(mu.reshape(-1, 1)).numpy()],
-        color='black',
-        lw=2
+        [np.min(dense_y), flow.prob(mu.reshape(-1, 1)).numpy()],
+        color="black",
+        lw=2,
     )
     ax.plot(
         [plus_sd, plus_sd],
         [np.min(dense_y), flow.prob(plus_sd.reshape(-1, 1)).numpy()],
-        '--',
-        color='green'
+        "--",
+        color="green",
     )
     ax.plot(
         [minus_sd, minus_sd],
         [np.min(dense_y), flow.prob(minus_sd.reshape(-1, 1)).numpy()],
-        '--',
-        color='green'
+        "--",
+        color="green",
     )
 
     def quant(p):
         q = bijector.forward(base_dist.quantile(p))
         return np.squeeze(q)
 
-    qs = [.05, .1, .2, .3, .4]
+    qs = [0.05, 0.1, 0.2, 0.3, 0.4]
     ax.fill_between(
-        np.squeeze(y),
-        np.squeeze(dense_y),
-        np.min(dense_y),
-        fc=color,
-        alpha=max(qs)
+        np.squeeze(y), np.squeeze(dense_y), np.min(dense_y), fc=color, alpha=max(qs)
     )
     for i, q in enumerate(sorted(qs)):
         ax.fill_between(
             np.squeeze(y),
             np.squeeze(dense_y),
             np.min(dense_y),
-            where=(
-                (np.squeeze(y) > quant(q)) & (np.squeeze(y) < quant(1 - q))
-            ),
+            where=((np.squeeze(y) > quant(q)) & (np.squeeze(y) < quant(1 - q))),
             fc=color,
-            alpha=q / max(qs)
+            alpha=q / max(qs),
         )
 
-    ax.plot(
-        y,
-        dense_y,
-        '-',
-        color=color,
-        linewidth=2
-    )
+    ax.plot(y, dense_y, "-", color=color, linewidth=2)
