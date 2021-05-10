b4d20d0b0b0326810846e060e06e957607a5c996
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-05-10 11:29:24 +0200

    adds linear extrapolation

---
+++ src/bernstein_flow/bijectors/bernstein_extra.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+from . import BernsteinBijector
+
+
+class BernsteinBijectorLinearExtrapolate(BernsteinBijector):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        # save slope on boundaries for interpolation
+        self.z_min = tf.math.reduce_min(self.thetas, axis=-1)
+        self.z_max = tf.math.reduce_max(self.thetas, axis=-1)
+
+        a = tf.cast(self.order - 1, self.thetas.dtype)
+        self.a0 = a * (self.thetas[..., 1] - self.thetas[..., 0])
+        self.b0 = self.z_min
+
+        self.a1 = a * (self.thetas[..., -1] - self.thetas[..., -2])
+        self.b1 = self.z_max
+
+        self.ldj0 = tf.math.log(self.a0)
+        self.ldj1 = tf.math.log(self.a1)
+
+    def _extrapolate_forward(self, y, z):
+        # z = a * y + b
+        e0 = self.a0 * y + self.b0
+        e1 = self.a1 * (y - 1) + self.b1
+
+        z = tf.where(y <= 0, e0, z)
+        z = tf.where(y >= 1, e1, z)
+
+        return z
+
+    def _extrapolate_inverse(self, z, y):
+        # y = (z - b) / a
+        e0 = (z - self.b0) / self.a0
+        e1 = (z - self.b1) / self.a1 + 1
+
+        y = tf.where(z <= self.z_min, e0, y)
+        y = tf.where(z >= self.z_max, e1, y)
+
+        return y
+
+    def _extrapolate_forward_log_det_jacobian(self, y, ldj):
+        # dz = a
+        ldj = tf.where(y <= 0.0, self.ldj0, ldj)
+        ldj = tf.where(y >= 1.0, self.ldj1, ldj)
+        return ldj
+
+    def _inverse(self, z: tf.Tensor) -> tf.Tensor:
+        """
+        Returns the inverse Bijector evaluation.
+
+        :param      z:    The input to the inverse evaluation.
+        :type       z:    Tensor
+
+        :returns:   The inverse Bijector evaluation.
+        :rtype:     Tensor
+        """
+        y = super()._inverse(tf.where((z <= self.z_min) | (z >= self.z_max), 0.0, z))
+        return self._extrapolate_inverse(z, y)
+
+    def _forward(self, y: tf.Tensor) -> tf.Tensor:
+        """
+        Returns the forward Bijector evaluation.
+
+        :param      y:    The input to the forward evaluation.
+        :type       y:    Tensor
+
+        :returns:   The forward Bijector evaluation.
+        :rtype:     Tensor
+        """
+        # Note: If the gradient of either branch of the tf.where generates a NaN,
+        #       then the gradient of the entire tf.where will be NaN.
+        #       -> use an inner tf.where to ensure the function has no asymptote
+        z = super()._forward(tf.where((y <= 0) | (y >= 1), tf.ones_like(y), y))
+
+        z = self._extrapolate_forward(y, z)
+        return z
+
+    def _forward_log_det_jacobian(self, y):
+        ldj = super()._forward_log_det_jacobian(tf.where((y <= 0) | (y >= 1), 0.0, y))
+        ldj = self._extrapolate_forward_log_det_jacobian(y, ldj)
+
+        return ldj
---
+++ test/bijectors/test_bernstein_bijector_extra.py
@@ -0,0 +1,70 @@
+#!env python3
+# AUTHOR INFORMATION ##########################################################
+# file    : test_bernstein_bijector.py
+# brief   : [Description]
+#
+# author  : Marcel Arpogaus
+# created : 2020-10-16 08:12:04
+# changed : 2020-11-23 18:03:28
+# DESCRIPTION #################################################################
+#
+# This project is following the PEP8 style guide:
+#
+#    https://www.python.org/dev/peps/pep-0008/)
+#
+# LICENSE #####################################################################
+# Copyright 2020 Marcel Arpogaus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# REQUIRED PYTHON MODULES #####################################################
+import numpy as np
+import tensorflow as tf
+
+from bernstein_flow.bijectors import (
+    BernsteinBijectorLinearExtrapolate as BernsteinBijector,
+)
+
+
+class BernsteinBijectorTest(tf.test.TestCase):
+    def test_inverse(self, batch_shape=[], x_shape=[100], order=10):
+        thetas = BernsteinBijector.constrain_theta(
+            np.ones(batch_shape + [order]).astype(np.float32)
+        )
+        print(thetas)
+        x = np.float32(np.random.uniform(0 + 1e-2, 1 - 1e-2, x_shape))
+
+        bb = BernsteinBijector(thetas=thetas)
+
+        forward_x = bb.forward(x)
+        # Use identity to invalidate cache.
+        inverse_x = bb.inverse(tf.identity(forward_x))
+        forward_inverse_x = bb.forward(inverse_x)
+
+        fldj = bb.forward_log_det_jacobian(x, event_ndims=1)
+        # Use identity to invalidate cache.
+        ildj = bb.inverse_log_det_jacobian(tf.identity(forward_x), event_ndims=1)
+
+        self.assertAllClose(x, inverse_x, rtol=1e-5, atol=1e-4)
+        self.assertAllClose(forward_x, forward_inverse_x, rtol=1e-5, atol=1e-4)
+        self.assertAllClose(ildj, -fldj, rtol=1e-5, atol=0.0)
+
+    def test_inverse_batched(self):
+        self.test_inverse(batch_shape=[2], x_shape=[100, 2])
+
+    def test_inverse_batched_multi(self):
+        self.test_inverse(batch_shape=[2, 4], x_shape=[100, 2, 4])
+
+    def test_inverse_batched_multi_huge(self):
+        self.test_inverse(batch_shape=[16, 48], x_shape=[100, 16, 48])
