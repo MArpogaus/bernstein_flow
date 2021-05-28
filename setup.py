e1a9922fb438bac38137082659444e6160b0630e
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-05-28 15:24:07 +0200

    specifies pkg versions

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="bernstein_flow",
     version="0.0.1",
-    packages=find_packages('src'),
+    packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        'matplotlib',
-        'numpy',
-        'scipy',
-        'tensorflow',
-        'tensorflow_probability',
-    ]
+        "tensorflow==2.4.1",
+        "tensorflow_probability==0.12.2",
+        "matplotlib==3.3.4",
+        "seaborn==0.11.1",
+        "numpy==1.19.3",
+        "scipy==1.4.1",
+    ],
 )
