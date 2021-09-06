97a1208799b29e921e73b7ffa2c91db09f8da710
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-09-06 16:25:21 +0200

    bumped tf version to 2.5.*

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "tensorflow==2.4.1",
-        "tensorflow_probability==0.12.2",
-        "matplotlib==3.3.4",
-        "seaborn==0.11.1",
-        "numpy==1.19.3",
-        "scipy==1.4.1",
+        "tensorflow==2.5.*",
+        "tensorflow_probability==0.13.*",
+        "scipy==1.7.*",
+        "matplotlib",
+        "seaborn",
     ],
 )
