41861354c4a28b153bc7d366aeab85bc2cf52bce
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-10-14 16:26:43 +0200

    bumps tf and tfp version

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "tensorflow==2.5.*",
-        "tensorflow_probability==0.13.*",
+        "tensorflow==2.6.*",
+        "tensorflow_probability==0.14.*",
         "scipy==1.7.*",
         "matplotlib",
         "seaborn",
