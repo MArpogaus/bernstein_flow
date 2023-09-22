ff848147fb37e4cb50adf37628c29623466a843a
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2023-09-22 16:06:21 +0200

    allows to define upper and lower bound separately

--- src/bernstein_flow/activations/__init__.py
+++ src/bernstein_flow/activations/__init__.py
@@ -19,8 +19,8 @@ from tensorflow_probability.python.internal import (
 
 
 def get_thetas_constrain_fn(
-    low=-4,
-    high=4,
+    low=None,
+    high=None,
     smooth_bounds=False,
     allow_flexible_bounds=False,
     fn=tf.math.softplus,
@@ -30,17 +30,28 @@ def get_thetas_constrain_fn(
         dtype = dtype_util.common_dtype([diff], dtype_hint=tf.float32)
 
         diff = tensor_util.convert_nonref_to_tensor(diff, name="diff", dtype=dtype)
-        low_theta = tensor_util.convert_nonref_to_tensor(low, name="low", dtype=dtype)
-        high_theta = tensor_util.convert_nonref_to_tensor(
-            high, name="high", dtype=dtype
-        )
+        low_theta, high_theta = None, None
+        if low is not None:
+            low_theta = tensor_util.convert_nonref_to_tensor(
+                low, name="low", dtype=dtype
+            ) * tf.ones_like(diff[..., :1])
+        else:
+            low_theta = diff[..., :1]
+            diff = diff[..., 1:]
+
+        if high is not None:
+            high_theta = tensor_util.convert_nonref_to_tensor(
+                high, name="high", dtype=dtype
+            )
 
         if allow_flexible_bounds:
-            low_theta -= fn(diff[..., :1])
-            high_theta += fn(diff[..., -1:])
-            diff = diff[..., 1:-1]
+            if tf.is_tensor(low_theta):
+                low_theta -= fn(diff[..., :1])
+                diff = diff[..., 1:]
+            if tf.is_tensor(high_theta):
+                high_theta += fn(diff[..., :1])
+                diff = diff[..., 1:]
 
-        theta0 = low_theta * tf.ones_like(diff[..., :1])
         diff_positive = fn(diff)
 
         if smooth_bounds:
@@ -56,17 +67,18 @@ def get_thetas_constrain_fn(
             )
 
         diff_positive = tf.maximum(diff_positive, eps)
-        diff_positive /= tf.reduce_sum(diff_positive, -1)[..., None]
-        diff_positive *= (
-            high_theta
-            - low_theta
-            - tf.cast(prefer_static.dimension_size(diff_positive, -1) + 1, dtype) * eps
-        )
-        diff_positive += eps
+        if tf.is_tensor(high_theta):
+            diff_positive /= tf.reduce_sum(diff_positive, -1)[..., None]
+            diff_positive *= (
+                high_theta
+                - low_theta
+                - tf.cast(prefer_static.dimension_size(diff_positive, -1), dtype) * eps
+            )
+            diff_positive += eps
 
         c = tf.concat(
             (
-                theta0,
+                low_theta,
                 diff_positive,
             ),
             axis=-1,
