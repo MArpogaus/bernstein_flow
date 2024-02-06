679bf6323ce9e72af17b8723d2294366cd64e67f
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-02-06 12:45:26 +0100

    merges get_thetas_constrain_fn from dev

--- src/bernstein_flow/activations/__init__.py
+++ src/bernstein_flow/activations/__init__.py
@@ -4,97 +4,83 @@
 # author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 #
 # created : 2022-03-10 15:39:04 (Marcel Arpogaus)
-# changed : 2024-02-05 15:04:16 (Marcel Arpogaus)
+# changed : 2024-02-06 11:55:20 (Marcel Arpogaus)
 # DESCRIPTION #################################################################
 # ...
 # LICENSE #####################################################################
 # ...
 ###############################################################################
-
 import tensorflow as tf
-from tensorflow_probability.python.internal import dtype_util, tensor_util
+from tensorflow_probability.python.internal import (
+    dtype_util,
+    prefer_static,
+    tensor_util,
+)
 
 
 def get_thetas_constrain_fn(
-    support=(-3, 3),
-    fn=tf.abs,
-    allow_values_outside_support=False,
-    constrain_second_drivative=False,
+    low=-4,
+    high=4,
+    smooth_bounds=False,
+    allow_flexible_bounds=False,
+    fn=tf.math.softplus,
+    eps=1e-5,
 ):
-    """Ensures monotone increasing Bernstein coefficients.
-
-    :param support: Support of the base distribution.
-      Parameters are scaled to this range.
-    :param fn: Function to ensure positive values.
-    :param allow_values_outside_support: Use the first and last parameter to
-      increase/decrease the upper/lower bound.
-    :param constrain_second_drivative: Apply extra contain w.r.t. second derivative
-      of the polynomial. Possible values are:
-       - None: dont apply any constrain
-       - zero: force second derivative to be zero on boundaries
-       - turn: force a turning-point on the boundaries
-    :returns:   Monotone increasing Bernstein coefficients.
-    :rtype:     Tensor
-
-    """
-
+    # @tf.function
     def constrain_fn(diff):
         dtype = dtype_util.common_dtype([diff], dtype_hint=tf.float32)
         diff = tensor_util.convert_nonref_to_tensor(diff, name="diff", dtype=dtype)
-        eps = dtype_util.eps(diff.dtype)
 
-        if support and len(support) == 2:
-            low, high = support
+        if low is not None:
+            low_theta = tensor_util.convert_nonref_to_tensor(
+                low, name="low", dtype=dtype
+            ) * tf.ones_like(diff[..., :1])
 
-            low = tensor_util.convert_nonref_to_tensor(low, name="low", dtype=dtype)
-            high = tensor_util.convert_nonref_to_tensor(high, name="high", dtype=dtype)
-
-            if allow_values_outside_support:
-                low -= fn(diff[..., :1], name="low")
-                high += fn(diff[..., -1:], name="high")
-                diff = diff[..., 1:-1]
-
-            theta0 = low * tf.ones_like(diff[..., :1])
+            if allow_flexible_bounds:
+                low_theta -= fn(diff[..., :1])
+                diff = diff[..., 1:]
         else:
-            theta0 = diff[..., :1]
+            low_theta = diff[..., :1]
             diff = diff[..., 1:]
 
-        diff_positive = tf.math.maximum(fn(diff, name="diff_positive"), eps)
+        if high is not None:
+            high_theta = tensor_util.convert_nonref_to_tensor(
+                high, name="high", dtype=dtype
+            )
 
-        if constrain_second_drivative:
-            if constrain_second_drivative == "zero":
-                low2 = diff_positive[..., :1]
-                high2 = diff_positive[..., -1:]
-            elif constrain_second_drivative == "turn":
-                diff_positive = diff_positive[..., 1:-1]
+            if allow_flexible_bounds:
+                high_theta += fn(diff[..., :1])
+                diff = diff[..., 1:]
 
-                low2 = diff_positive[..., :1] + fn(diff[..., :1])
-                high2 = diff_positive[..., -1:] + fn(diff[..., -1:])
-            else:
-                raise ValueError(
-                    f'Unsupported value "{constrain_second_drivative}" '
-                    "for constrain_second_drivative"
-                )
-
+        diff_positive = fn(diff)
+        if smooth_bounds:
             diff_positive = tf.concat(
                 (
-                    low2,
+                    diff_positive[..., :1],
                     diff_positive,
-                    high2,
+                    diff_positive[..., -1:],
                 ),
                 axis=-1,
             )
 
-        if support:
-            diff_positive /= tf.math.reduce_sum(diff_positive, axis=-1)[..., None]
-            diff_positive *= high - low
-        tc = tf.concat(
+        if high is not None:
+            diff_positive /= tf.reduce_sum(diff_positive, -1)[..., None]
+            diff_positive *= (
+                high_theta
+                - low_theta
+                - tf.cast(prefer_static.dimension_size(diff_positive, -1) + 1, dtype)
+                * eps
+            )
+            # diff_positive += eps
+
+        c = tf.concat(
             (
-                theta0,
-                diff_positive,
+                low_theta,
+                diff_positive + eps,
             ),
             axis=-1,
         )
-        return tf.cumsum(tc, axis=-1, name="theta")
+        thetas_constrained = tf.cumsum(c, axis=-1, name="theta")
+        return thetas_constrained
 
     return constrain_fn
