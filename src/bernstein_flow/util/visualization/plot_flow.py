1736fcd8698d6ae6c4d53d64a834e53ce45f506b
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-06-13 22:08:57 +0200

    refactors plot flow

--- src/bernstein_flow/util/visualization/plot_flow.py
+++ src/bernstein_flow/util/visualization/plot_flow.py
@@ -4,7 +4,7 @@
 # author  : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 #
 # created : 2022-06-01 15:21:22 (Marcel Arpogaus)
-# changed : 2024-06-13 21:27:07 (Marcel Arpogaus)
+# changed : 2024-06-13 22:08:38 (Marcel Arpogaus)
 # DESCRIPTION #################################################################
 #
 # This project is following the PEP8 style guide:
@@ -27,7 +27,7 @@
 # limitations under the License.
 ###############################################################################
 # REQUIRED PYTHON MODULES #####################################################
-from functools import partial, reduce
+from functools import partial
 from typing import Dict, List, Tuple
 
 import matplotlib as mpl
@@ -47,7 +47,7 @@ np.random.seed(42)
 tf.random.set_seed(42)
 
 
-def get_annot_map(bijector_names: List[str], bijector_name: str, cnt: int = 0) -> Dict:
+def get_annot_map(bijector_names: List[str], bijector_name: str) -> Dict[str, str]:
     """Get a map from bijector names to annotations.
 
     Parameters
@@ -55,26 +55,19 @@ def get_annot_map(bijector_names: List[str], bijector_name: str, cnt: int = 0) -
     bijector_names : List[str]
         List of bijector names.
     bijector_name : str
-        Name of the bijector to split the list at.
-    cnt : int, optional
-        Counter for the annotations, by default 0
+        Name of the bijector to split the data at.
 
     Returns
     -------
-    Dict
+    Dict[str, str]
         Dictionary mapping bijector names to annotations.
     """
-
-    def annot_map_mapper(name: str) -> Tuple[str, str]:
-        nonlocal cnt
+    annot_map = {}
+    for cnt, name in enumerate(bijector_names, 1):
         if name == "sigmoid":
-            annot = r"$\sigma$"
+            annot_map[name] = r"$\sigma$"
         else:
-            cnt += 1
-            annot = f"$f_{{{cnt}}}$"
-        return (name, annot)
-
-    annot_map = dict(map(annot_map_mapper, bijector_names))
+            annot_map[name] = f"$f_{{{cnt}}}$"
     annot_map.update({"bijector": annot_map[bijector_name]})
     return annot_map
 
@@ -100,18 +93,37 @@ def get_formulas(bijectors: List[tfb.Bijector]) -> str:
     str
         LaTeX formulas string.
     """
-    formuals = r"\begin{align*}"
-    cnt = 1
-    for b in reversed(bijectors):
-        if b.__class__ in FORMULAS.keys():
-            formuals += (
-                rf"  f_{{{cnt}}}: "
-                + FORMULAS[b.__class__].format(curr=cnt, prev=cnt - 1)
+    formulas = r"\begin{align*}"
+    for i, b in enumerate(reversed(bijectors)):
+        if b.__class__ in FORMULAS:
+            formulas += (
+                rf"  f_{{{i + 1}}}: "
+                + FORMULAS[b.__class__].format(curr=i + 1, prev=i)
                 + r"\\"
             )
-            cnt += 1
-    formuals += r"\end{align*}"
-    return formuals
+    formulas += r"\end{align*}"
+    return formulas
+
+
+def _get_bijectors_recursive(bijector: tfb.Bijector) -> List[tfb.Bijector]:
+    """Recursively extract bijectors from a (potentially chained) bijector.
+
+    Parameters
+    ----------
+    bijector : tfb.Bijector
+        The bijector to extract from.
+
+    Returns
+    -------
+    List[tfb.Bijector]
+        List of extracted bijectors.
+    """
+    if hasattr(bijector, "bijectors"):
+        return sum([_get_bijectors_recursive(b) for b in bijector.bijectors], [])
+    elif hasattr(bijector, "bijector"):
+        return _get_bijectors_recursive(bijector.bijector)
+    else:
+        return [bijector]
 
 
 def get_bijectors(
@@ -129,7 +141,7 @@ def get_bijectors(
     List[tfb.Bijector]
         List of bijectors.
     """
-    return flow.bijector.bijector.bijectors
+    return _get_bijectors_recursive(flow.bijector)
 
 
 def get_bijector_names(bijectors: List[tfb.Bijector]) -> List[str]:
@@ -145,7 +157,7 @@ def get_bijector_names(bijectors: List[tfb.Bijector]) -> List[str]:
     List[str]
         List of bijector names.
     """
-    return list(map(lambda x: x.name, reversed(bijectors)))
+    return [b.name for b in reversed(bijectors)]
 
 
 def split_bijector_names(
@@ -165,31 +177,8 @@ def split_bijector_names(
     Tuple[List[str], List[str]]
         Tuple containing the two split lists.
     """
-    bpoly_idx = bijector_names.index(split_bijector_name) + 1
-    return bijector_names[:bpoly_idx], bijector_names[bpoly_idx:]
-
-
-def get_intersec_reducer(arr: List) -> callable:
-    """Generate a reducer function to count intersections.
-
-    Parameters
-    ----------
-    arr : List
-        List to check for intersections.
-
-    Returns
-    -------
-    callable
-        Reducer function.
-    """
-
-    def reducer(c: int, i: int) -> int:
-        if i in arr:
-            return c + 1
-        else:
-            return c
-
-    return reducer
+    split_index = bijector_names.index(split_bijector_name) + 1
+    return bijector_names[:split_index], bijector_names[split_index:]
 
 
 def get_plot_data(
@@ -198,7 +187,7 @@ def get_plot_data(
     n: int = 200,
     z_values: np.ndarray = None,
     seed: int = 1,
-) -> Tuple[Dict, List[str], List[str]]:
+) -> Tuple[Dict[str, Dict[str, np.ndarray]], List[str], List[str]]:
     """Generate plot data for a transformed distribution.
 
     Parameters
@@ -216,7 +205,7 @@ def get_plot_data(
 
     Returns
     -------
-    Tuple[Dict, List[str], List[str]]
+    Tuple[Dict[str, Dict[str, np.ndarray]], List[str], List[str]]
         Tuple containing the plot data, post-split bijector names,
         and pre-split bijector names.
     """
@@ -236,22 +225,25 @@ def get_plot_data(
 
     log_probs = base_dist.log_prob(z_sorted).numpy()
 
-    zz = z_sorted[..., None]
+    plot_data: Dict[str, Dict[str, np.ndarray]] = {
+        "distribution": {"z": z_sorted, "p": np.exp(log_probs)}
+    }
+    z = z_sorted[..., None]
     ildj = 0.0
-    plot_data = {"distribution": dict(z=z_sorted, p=np.exp(log_probs))}
 
     for i, b in enumerate(chained_bijectors):
-        # we need to use the inverse here since we are going from z->y!
-        z = b.inverse(zz).numpy()
+        z = b.inverse(z).numpy()
         ildj += b.forward_log_det_jacobian(z, 1)
         name = b.name
-        plot_data[name] = dict(z=z, p=np.exp(log_probs + ildj))
-        zz = z
+        plot_data[name] = {"z": z, "p": np.exp(log_probs + ildj)}
 
-    after_bpoly = next(iter(post_bpoly_trafos), "distribution")
-    z1 = plot_data[bijector_name]["z"]
-    z2 = plot_data[after_bpoly]["z"]
-    plot_data["bijector"] = dict(z1=z1, z2=z2)
+    after_bpoly = next(
+        (name for name in post_bpoly_trafos if name in plot_data), "distribution"
+    )
+    plot_data["bijector"] = {
+        "z1": plot_data[bijector_name]["z"],
+        "z2": plot_data[after_bpoly]["z"],
+    }
     return plot_data, post_bpoly_trafos, pre_bpoly_trafos
 
 
@@ -266,18 +258,16 @@ def configure_axes(a: Axes, style: str):
         Style of the axes. Can be "right", "top", or "none".
     """
     if style == "right":
-        a.spines["top"].set_color("none")  # don't draw spine
-        # a.spines["right"].set_color("none")  # don't draw spine
-        a.spines["bottom"].set_color("none")  # don't draw spine
-        a.spines["left"].set_color("none")  # don't draw spine
+        a.spines["top"].set_color("none")
+        a.spines["bottom"].set_color("none")
+        a.spines["left"].set_color("none")
         a.get_xaxis().set_visible(False)
         a.get_yaxis().tick_right()
         a.get_yaxis().set_label_coords(1, 1.05)
     elif style == "top":
-        # a.spines["top"].set_color("none")  # don't draw spine
-        a.spines["right"].set_color("none")  # don't draw spine
-        a.spines["bottom"].set_color("none")  # don't draw spine
-        a.spines["left"].set_color("none")  # don't draw spine
+        a.spines["right"].set_color("none")
+        a.spines["bottom"].set_color("none")
+        a.spines["left"].set_color("none")
         a.get_yaxis().set_visible(False)
         a.get_xaxis().tick_top()
         a.get_xaxis().set_label_coords(1.05, 1)
@@ -287,7 +277,7 @@ def configure_axes(a: Axes, style: str):
 
 
 def prepare_figure(
-    plot_data: Dict,
+    plot_data: Dict[str, Dict[str, np.ndarray]],
     pre_bpoly_trafos: List[str],
     post_bpoly_trafos: List[str],
     size: int = 4,
@@ -298,7 +288,7 @@ def prepare_figure(
 
     Parameters
     ----------
-    plot_data : Dict
+    plot_data : Dict[str, Dict[str, np.ndarray]]
         Plot data.
     pre_bpoly_trafos : List[str]
         Pre-split bijector names.
@@ -316,8 +306,8 @@ def prepare_figure(
     Tuple[Figure, Dict[str, Axes]]
         Tuple containing the figure and a dictionary mapping bijector names to axes.
     """
-    pre_bpoly = reduce(get_intersec_reducer(pre_bpoly_trafos), plot_data.keys(), 0)
-    post_bpoly = reduce(get_intersec_reducer(post_bpoly_trafos), plot_data.keys(), 0)
+    pre_bpoly = sum(k in pre_bpoly_trafos for k in plot_data)
+    post_bpoly = sum(k in post_bpoly_trafos for k in plot_data)
 
     fig = plt.figure(figsize=((pre_bpoly + 1) * size, (post_bpoly + 2) * size))
     gs0 = fig.add_gridspec(
@@ -339,12 +329,13 @@ def prepare_figure(
     gs11 = gs0[1, 1].subgridspec(post_bpoly + 1, 1, hspace=hspace)
     gs11_it = iter(gs11)
 
-    axs = {}
+    axs: Dict[str, Axes] = {}
     axs["bijector"] = fig.add_subplot(gs0[0, 1])
     configure_axes(axs["bijector"], "none")
+
     idx = 0
     for k in pre_bpoly_trafos + post_bpoly_trafos + ["distribution"]:
-        if k not in plot_data.keys():
+        if k not in plot_data:
             continue
         if k in pre_bpoly_trafos:
             axs[k] = fig.add_subplot(next(gs00_it), sharey=axs["bijector"])
@@ -354,10 +345,7 @@ def prepare_figure(
             axs[k] = fig.add_subplot(next(gs11_it), sharex=axs["bijector"])
             configure_axes(axs[k], "top")
             set_label = partial(axs[k].set_xlabel, va="center", ha="left")
-        if idx == 0:
-            set_label("$y$")
-        else:
-            set_label(f"$z_{{{idx}}}$")
+        set_label("$y$" if idx == 0 else f"$z_{{{idx}}}$")
         idx += 1
 
     axs["math"] = fig.add_subplot(gs0[1, 0])
@@ -368,7 +356,7 @@ def prepare_figure(
 
 def plot_data_to_axes(
     axs: Dict[str, Axes],
-    plot_data: Dict,
+    plot_data: Dict[str, Dict[str, np.ndarray]],
     pre_bpoly_trafos: List[str],
     post_bpoly_trafos: List[str],
 ):
@@ -378,7 +366,7 @@ def plot_data_to_axes(
     ----------
     axs : Dict[str, Axes]
         Dictionary mapping bijector names to axes.
-    plot_data : Dict
+    plot_data : Dict[str, Dict[str, np.ndarray]]
         Plot data.
     pre_bpoly_trafos : List[str]
         Pre-split bijector names.
@@ -413,21 +401,18 @@ def plot_data_to_axes(
             )
             ax.invert_yaxis()
         elif k == "bijector":
-            ax = axs[k]
-            z1 = plot_data[k]["z1"]
-            z2 = plot_data[k]["z2"]
-            ax.scatter(z2, z1, c="orange", s=4)
+            ax.scatter(v["z2"], v["z1"], c="orange", s=4)
 
 
 def add_annot_to_axes(
     axs: Dict[str, Axes],
-    plot_data: Dict,
+    plot_data: Dict[str, Dict[str, np.ndarray]],
     pre_bpoly_trafos: List[str],
     post_bpoly_trafos: List[str],
     bijector_name: str,
-    annot_map: Dict = {},
-    extra_annot_prob: Dict = {},
-    extra_annot_sample: Dict = {},
+    annot_map: Dict[str, str] = {},
+    extra_annot_prob: Dict[str, Tuple[Tuple[float, float], str, int]] = {},
+    extra_annot_sample: Dict[str, Tuple[Tuple[float, float], str, int]] = {},
     formulas: str = "",
     pos: float = 0.5,
     cp_kwds: Dict = dict(arrowstyle="-|>", shrinkA=10, shrinkB=10, color="gray"),
@@ -439,7 +424,7 @@ def add_annot_to_axes(
     ----------
     axs : Dict[str, Axes]
         Dictionary mapping bijector names to axes.
-    plot_data : Dict
+    plot_data : Dict[str, Dict[str, np.ndarray]]
         Plot data.
     pre_bpoly_trafos : List[str]
         Pre-split bijector names.
@@ -447,11 +432,11 @@ def add_annot_to_axes(
         Post-split bijector names.
     bijector_name : str
         Name of the bijector to split the data at.
-    annot_map : Dict, optional
+    annot_map : Dict[str, str], optional
         Dictionary mapping bijector names to annotations, by default {}
-    extra_annot_prob : Dict, optional
+    extra_annot_prob : Dict[str, Tuple[Tuple[float, float], str, int]], optional
         Dictionary containing extra annotations for probabilities, by default {}
-    extra_annot_sample : Dict, optional
+    extra_annot_sample : Dict[str, Tuple[Tuple[float, float], str, int]], optional
         Dictionary containing extra annotations for samples, by default {}
     formulas : str, optional
         LaTeX formulas string, by default ""
@@ -463,7 +448,6 @@ def add_annot_to_axes(
     usetex : bool, optional
         Whether to use LaTeX for text rendering, by default True
     """
-    # add arrows for bijectors
     xyA = None
     axA = None
     z1 = plot_data["bijector"]["z1"]
@@ -472,31 +456,19 @@ def add_annot_to_axes(
     z1 = z1[i].item()
     z2 = z2[i].item()
     ax_names = pre_bpoly_trafos + ["bijector"] + post_bpoly_trafos + ["distribution"]
-    ax_names = list(filter(lambda x: x in plot_data.keys(), ax_names))
+    ax_names = [x for x in ax_names if x in plot_data]
     for kA, kB in zip(ax_names[:-1], ax_names[1:]):
         axA = axs[kA]
         axB = axs[kB]
         if kA == bijector_name:
             xyB = (z2, z1)
-            kwdsText = dict(
-                xy=(0.5, 0),
-                ha="center",
-                va="top",
-            )
+            kwdsText = dict(xy=(0.5, 0), ha="center", va="top")
         elif kA in pre_bpoly_trafos:
             xyB = (0, z1)
-            kwdsText = dict(
-                xy=(0.5, 0),
-                ha="center",
-                va="top",
-            )
+            kwdsText = dict(xy=(0.5, 0), ha="center", va="top")
         elif kA in post_bpoly_trafos + ["bijector", "distribution"]:
             xyB = (z2, 0)
-            kwdsText = dict(
-                xy=(0.5, 0.5),
-                ha="left",
-                va="center",
-            )
+            kwdsText = dict(xy=(0.5, 0.5), ha="left", va="center")
 
         cp = ConnectionPatch(
             xyA=xyA or xyB,
@@ -513,9 +485,7 @@ def add_annot_to_axes(
         axB.figure.add_artist(tx)
         xyA = xyB
 
-    # add formulas
-    ax = axs["math"]
-    ax.text(0.5, 0.5, formulas, ha="center", va="center", usetex=usetex)
+    axs["math"].text(0.5, 0.5, formulas, ha="center", va="center", usetex=usetex)
 
     common_arrowprops = dict(
         arrowstyle="-",
@@ -524,15 +494,13 @@ def add_annot_to_axes(
         facecolor="black",
     )
 
-    # annotate Bernstein polinomial
     k = "bijector"
     z1 = plot_data[k]["z1"]
     z2 = plot_data[k]["z2"]
     i = int(len(z1) * 0.9)
     z1 = z1[i].item()
     z2 = z2[i].item()
-    ax = axs[k]
-    ax.annotate(
+    axs[k].annotate(
         "Bernstein\nPolynomial",
         xy=(z2, z1),
         xycoords="data",
@@ -547,17 +515,15 @@ def add_annot_to_axes(
     for k, (xytext, text, i) in extra_annot_prob.items():
         z = plot_data[k]["z"]
         p = plot_data[k]["p"]
-        arrowprops = common_arrowprops.copy()
         if k in pre_bpoly_trafos:
             x = p[i].item()
             y = z[i].item()
-            arrowprops["connectionstyle"] = "arc3,rad=-0.5"
+            connectionstyle = "arc3,rad=-0.5"
         else:
             x = z[i].item()
             y = p[i].item()
-            arrowprops["connectionstyle"] = "arc3,rad=0.5"
-        ax = axs[k]
-        ax.annotate(
+            connectionstyle = "arc3,rad=0.5"
+        axs[k].annotate(
             text,
             xy=(x, y),
             xycoords="data",
@@ -566,22 +532,20 @@ def add_annot_to_axes(
             ha="right",
             va="center",
             usetex=usetex,
-            arrowprops=arrowprops,
+            arrowprops=dict(**common_arrowprops, connectionstyle=connectionstyle),
         )
 
     for k, (xytext, text, i) in extra_annot_sample.items():
         z = plot_data[k]["z"]
-        arrowprops = common_arrowprops.copy()
         if k in pre_bpoly_trafos:
             x = 0
             y = z[i].item()
-            arrowprops["connectionstyle"] = "arc3,rad=-0.5"
+            connectionstyle = "arc3,rad=-0.5"
         else:
             x = z[i].item()
             y = 0
-            arrowprops["connectionstyle"] = "arc3,rad=0.5"
-        ax = axs[k]
-        ax.annotate(
+            connectionstyle = "arc3,rad=0.5"
+        axs[k].annotate(
             text,
             xy=(x, y),
             xycoords="data",
@@ -590,13 +554,13 @@ def add_annot_to_axes(
             ha="right",
             va="center",
             usetex=usetex,
-            arrowprops=arrowprops,
+            arrowprops=dict(**common_arrowprops, connectionstyle=connectionstyle),
         )
 
 
 def plot_flow(
     flow: tfp.distributions.TransformedDistribution,
-    bijector_name: str = "bpoly",
+    bijector_name: str = "bernstein_bijector",
     n: int = 500,
     z_values: np.ndarray = None,
     size: float = 1.5,
@@ -610,7 +574,7 @@ def plot_flow(
     flow : tfp.distributions.TransformedDistribution
         Transformed distribution to plot.
     bijector_name : str, optional
-        Name of the bijector to split the data at, by default "bpoly"
+        Name of the bijector to split the data at, by default "bernstein_bijector"
     n : int, optional
         Number of samples, by default 500
     z_values : np.ndarray, optional
@@ -634,27 +598,20 @@ def plot_flow(
     """
     if usetex:
         plt.rcParams.update(
-            {
-                # for the align enivironment
-                "text.latex.preamble": r"\usepackage{amsmath}",
-                "text.usetex": True,  # use inline math for ticks
-            }
+            {"text.latex.preamble": r"\usepackage{amsmath}", "text.usetex": True}
         )
-    assert flow.batch_shape == [] or flow.batch_shape == [
-        1
-    ], "Only unimodal distributions supported"
+    assert flow.batch_shape in ([], [1]), "Only unimodal distributions supported"
     plot_data, post_bpoly_trafos, pre_bpoly_trafos = get_plot_data(
         flow, bijector_name=bijector_name, n=n, z_values=z_values
     )
     fig, axs = prepare_figure(plot_data, pre_bpoly_trafos, post_bpoly_trafos, size=size)
     plot_data_to_axes(axs, plot_data, pre_bpoly_trafos, post_bpoly_trafos)
     bijectors = get_bijectors(flow)
+    bijector_names = get_bijector_names(bijectors)
     add_annot_to_axes_kwds = {
         **dict(
             bijector_name=bijector_name,
-            annot_map=get_annot_map(
-                get_bijector_names(bijectors), bijector_name=bijector_name
-            ),
+            annot_map=get_annot_map(bijector_names, bijector_name),
             formulas=get_formulas(bijectors) if usetex else None,
             usetex=usetex,
         ),
