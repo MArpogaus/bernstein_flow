301250ecd7c47ef42fd4a12a2c4a47d8e140ee0a
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-07-12 15:18:53 +0200

    build: delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-# Only provided for legacy compatibility
-# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-#
-# Metadate is specified pyproject.toml
-
-from setuptools import setup
-
-setup()
